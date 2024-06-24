a8d5718ce78bbecb313214a039d6cc1c755cca35
jen444x <106173914+jen444x@users.noreply.github.com>
2024-06-24 15:29:38 -0700

    final product

--- js/script.js
+++ js/script.js
@@ -11,35 +11,6 @@ btnNavEl.addEventListener("click", function () {
 });
 
 ///////////////////////////////////////////////////////////
-// Smooth scrolling animation
-
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
-  });
-});
-
-///////////////////////////////////////////////////////////
 // Sticky navigation
 
 // const sectionHeroEl = document.querySelector(".section-hero");
@@ -172,3 +143,19 @@ const slider = function () {
   });
 };
 slider();
+
+/////////////////
+//close mobile nav
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    // e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
